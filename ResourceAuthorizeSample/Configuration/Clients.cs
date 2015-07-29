1445ccff9ff63378c77495600754e76db3d9e449
soliveira <soliveira@me.com>
2015-07-29 10:14:02 +0100

    Update Clients

---
+++ ResourceAuthorizeSample/Configuration/Clients.cs
@@ -0,0 +1,29 @@
+﻿namespace ResourceAuthorizeSample.Configuration
+{
+    using System.Collections.Generic;
+    using System.Runtime.InteropServices.WindowsRuntime;
+
+    using Thinktecture.IdentityServer.Core.Models;
+
+    public static class Clients
+    {
+        public static IEnumerable<Client> Get()
+        {
+            return new[]
+        {
+            new Client 
+            {
+                Enabled = true,
+                ClientName = "MVC Client",
+                ClientId = "mvc",
+                Flow = Flows.Implicit,
+                RedirectUris = new List<string>
+                {
+                    "http://localhost:59087/"
+                }
+            }
+        };
+        }
+
+    }
+}
